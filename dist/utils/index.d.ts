afcec85e4d1b6d3951c0d802092737e462e87ac0
Ulion <ulion2002@gmail.com>
2021-01-06 10:30:41 +0800

    gitpkg

---
+++ dist/utils/index.d.ts
@@ -0,0 +1,8 @@
+export * from './decoratePlugins';
+export * from './getInlineTypes';
+export * from './getVoidTypes';
+export * from './onDOMBeforeInputPlugins';
+export * from './onKeyDownPlugins';
+export * from './renderElementPlugins';
+export * from './renderLeafPlugins';
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/utils/renderLeafPlugins.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import { RenderLeafProps } from 'slate-react';
+import { RenderLeaf, SlatePlugin } from '../types';
+export declare const renderLeafPlugins: (plugins: SlatePlugin[], renderLeafList: RenderLeaf[]) => (leafProps: RenderLeafProps) => JSX.Element;
+//# sourceMappingURL=renderLeafPlugins.d.ts.map

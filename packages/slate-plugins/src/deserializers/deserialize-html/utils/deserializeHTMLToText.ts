0c7177812ca1b86268d6dc071f4525a685e066aa
Ulion <ulion2002@gmail.com>
2022-07-12 07:39:39 +0800

    span newline fix.

--- packages/slate-plugins/src/deserializers/deserialize-html/utils/deserializeHTMLToText.ts
+++ packages/slate-plugins/src/deserializers/deserialize-html/utils/deserializeHTMLToText.ts
@@ -3,6 +3,16 @@
  */
 export const deserializeHTMLToText = (node: HTMLElement | ChildNode) => {
   if (node.nodeType === Node.TEXT_NODE) {
-    return node.nodeValue === '\n' ? null : node.textContent;
+    if (node.nodeValue === '\n') {
+      const parentNode = node.parentElement!
+      if (parentNode.nodeName === 'SPAN') {
+        const whiteSpace = getComputedStyle(parentNode).whiteSpace || parentNode.style.whiteSpace
+        if (whiteSpace.slice(0, 3) === 'pre' || whiteSpace === 'break-spaces') {
+          return node.textContent
+        }
+      }
+      return null
+    }
+    return node.textContent;
   }
 };
